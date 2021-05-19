8fb571007a47221ca79b05a6ff0dd56c25a0a395
rocky-tanaka <hiroki.i.tanaka@rakuten.com>
2021-05-19 11:55:19 +0900

    add limit in migration file

--- tanask/db/schema.rb
+++ tanask/db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_12_055431) do
+ActiveRecord::Schema.define(version: 2021_05_19_024455) do
 
   create_table "tasks", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.text "name", null: false
-    t.text "description", null: false
+    t.text "name", size: :tiny, null: false
+    t.text "description", size: :tiny, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
