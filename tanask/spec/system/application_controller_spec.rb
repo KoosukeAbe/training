2b33d3ad0cd43cde322255acd477c0007baf50c3
rocky-tanaka <hiroki.i.tanaka@rakuten.com>
2021-05-21 17:14:31 +0900

    add 500 test

--- tanask/spec/system/application_controller_spec.rb
+++ tanask/spec/system/application_controller_spec.rb
@@ -1,11 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe 'ApplicationControllers', js:true, type: :system do
+RSpec.describe 'ApplicationController', js:true, type: :system do
   describe 'error' do
     context 'If the user access unexist task URL' do
       it 'show 404 page' do
         visit 'tasks/hoge'
         expect(page).to have_content('404')
+      end
+    end
+    context 'If the raise 500 error' do
+      it 'show 500 page' do
+        allow(Task).to receive(:all).and_throw(ActiveModel::Errors)
+        visit 'tasks/'
+        expect(page).to have_content('500')
       end
     end
   end
