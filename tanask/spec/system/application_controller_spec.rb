d0f365cdbe7ada17e982dbf0dfb531bf217b9dae
rocky-tanaka <hiroki.i.tanaka@rakuten.com>
2021-05-21 11:58:26 +0900

    add 404 test

--- tanask/spec/system/application_controller_spec.rb
+++ tanask/spec/system/application_controller_spec.rb
@@ -5,11 +5,7 @@ RSpec.describe 'ApplicationControllers', js:true, type: :system do
     context 'If the user access unexist task URL' do
       it 'show 404 page' do
         visit 'tasks/hoge'
-        # expect(page.status_code).to eq 404
-        # expect(response.response_code).to eq(404)
-        # expect(response).to have_http_status(404)
-        # p(response)
-        expect(response).to assert_template(file: "#{Rails.root}/app/views/errors/404")
+        expect(page).to have_content('404')
       end
     end
   end
